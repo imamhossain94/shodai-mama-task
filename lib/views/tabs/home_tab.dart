ca87d5bfa5b6b6fa32862b266d523e603b09a734
imamhossain94 <imamagun94@gmail.com>
2022-01-11 19:31:54 +0600

    feat: slider image added

--- lib/views/tabs/home_tab.dart
+++ lib/views/tabs/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class HomeTab  extends StatelessWidget  {
@@ -5,12 +6,36 @@ class HomeTab  extends StatelessWidget  {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Home Tab",
-          style: TextStyle(fontSize: 20),
-        ),
+    return Scaffold(
+      body: CarouselSlider(
+          items: [1,2,3,4,5].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    //margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    decoration: BoxDecoration(
+                        color: Colors.amber
+                    ),
+                    child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+                );
+              },
+            );
+          }).toList(),
+          options: CarouselOptions(
+            //height: 400,
+            aspectRatio: 16/7,
+            viewportFraction: 0.8,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            enlargeCenterPage: false,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            scrollDirection: Axis.horizontal,
+          )
       ),
     );
   }
